30a0d9c7ac43dd9435fcd5036448e9890edc512b
ziram <menouerramzi0001@gmail.com>
2023-01-01 19:22:35 +0100

    001

--- pages/Orders.vue
+++ pages/Orders.vue
@@ -124,6 +124,200 @@
             </v-col>
         </v-row>
         </div>
+
+
+        <!-- start mobile theme -->
+
+        <v-data-iterator
+            :items="orders"
+            :items-per-page.sync="itemsPerPage"
+            :page.sync="page"
+            :search="search"
+            :sort-by="sortBy.toLowerCase()"
+            :sort-desc="sortDesc"
+            hide-default-footer
+            class="mx-4 my-4 d-sm-none d-block"
+            >
+            <template v-slot:header>
+                <v-toolbar
+                dark
+                color="blue darken-3"
+                class="mb-1"
+                >
+                <v-text-field
+                    v-model="search"
+                    clearable
+                    flat
+                    solo-inverted
+                    hide-details
+                    prepend-inner-icon="mdi-magnify"
+                    label="Search"
+                ></v-text-field>
+                <template v-if="$vuetify.breakpoint.mdAndUp">
+                    <v-spacer></v-spacer>
+                    <v-select
+                    v-model="sortBy"
+                    flat
+                    solo-inverted
+                    hide-details
+                    :items="keys"
+                    prepend-inner-icon="mdi-magnify"
+                    label="Sort by"
+                    ></v-select>
+                    <v-spacer></v-spacer>
+                    <v-btn-toggle
+                    v-model="sortDesc"
+                    mandatory
+                    >
+                    <v-btn
+                        large
+                        depressed
+                        color="blue"
+                        :value="false"
+                    >
+                        <v-icon>mdi-arrow-up</v-icon>
+                    </v-btn>
+                    <v-btn
+                        large
+                        depressed
+                        color="blue"
+                        :value="true"
+                    >
+                        <v-icon>mdi-arrow-down</v-icon>
+                    </v-btn>
+                    </v-btn-toggle>
+                </template>
+                </v-toolbar>
+            </template>
+
+            <template v-slot:default="props">
+                <v-row>
+                <v-col
+                    v-for="(item, index) in props.items"
+                    :key="index"
+                    cols="12"
+                    sm="6"
+                    md="4"
+                    lg="3"
+                >
+                    <v-card>
+                    <v-card-title class="subheading font-weight-bold">
+                        {{ item.client }}
+                    </v-card-title>
+
+                    <v-divider></v-divider>
+
+                    <v-list dense>
+                        <v-list-item
+                        v-for="(key, index) in filteredKeys"
+                        :key="index"
+                        >
+                        <v-list-item-content :class="{ 'blue--text': sortBy === key }">
+                            {{ key }}:
+                        </v-list-item-content>
+                        <v-list-item-content
+                            class="align-end"
+                            :class="{ 'blue--text': sortBy === key }"
+                        >
+                            {{ item[key.toLowerCase()] }}
+                        </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item>
+                            <v-list-item-content :class="{ 'blue--text': sortBy === key }">
+                                Action:
+                            </v-list-item-content>
+                     
+                            <v-icon  small class="mr-2" @click="(readonly = true) , editItem(item)">
+                                mdi-eye
+                            </v-icon>
+                            <v-icon v-if="item.statut == 'pending'" small class="mr-2" @click="(readonly = false) ,editItem(item)">
+                                mdi-pencil
+                            </v-icon>
+                            <v-icon v-if="item.statut == 'pending'" small class="mr-2" @click="deleteItem(item)">
+                                mdi-delete
+                            </v-icon>
+
+                        </v-list-item>
+                    </v-list>
+                    </v-card>
+                </v-col>
+                </v-row>
+            </template>
+
+            <template v-slot:footer>
+                <v-row
+                class="mt-2"
+                align="center"
+                justify="center"
+                >
+                <span class="grey--text">Items per page</span>
+                <v-menu offset-y>
+                    <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        dark
+                        text
+                        color="primary"
+                        class="ml-2"
+                        v-bind="attrs"
+                        v-on="on"
+                    >
+                        {{ itemsPerPage }}
+                        <v-icon>mdi-chevron-down</v-icon>
+                    </v-btn>
+                    </template>
+                    <v-list>
+                    <v-list-item
+                        v-for="(number, index) in itemsPerPageArray"
+                        :key="index"
+                        @click="updateItemsPerPage(number)"
+                    >
+                        <v-list-item-title>{{ number }}</v-list-item-title>
+                    </v-list-item>
+                    </v-list>
+                </v-menu>
+
+                <v-spacer></v-spacer>
+
+                <span
+                    class="mr-4
+                    grey--text"
+                >
+                    Page {{ page }} of {{ numberOfPages }}
+                </span>
+                <v-btn
+                    
+                    dark
+                    color="blue darken-3"
+                    class="mr-1"
+                    @click="formerPage"
+                >
+                    <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+                <v-btn
+                    
+                    dark
+                    color="blue darken-3"
+                    class="ml-1"
+                    @click="nextPage"
+                >
+                    <v-icon>mdi-chevron-right</v-icon>
+                </v-btn>
+                </v-row>
+            </template>
+            </v-data-iterator>
+
+        <!-- end mobile theme -->
+
+
+
+
+
+
+
+
+
+
+
         <v-data-table 
         :headers="headers" 
         :items="orders" 
@@ -132,7 +326,7 @@
         :search="searchOrder" 
         sort-by="$createdAt"
         sort-desc
-        class="elevation-1 rounded-xl">
+        class="elevation-1 rounded-xl d-none d-sm-block">
             <template v-slot:top>
                 <v-toolbar flat class="">
                     <v-text-field v-model="searchOrder" append-icon="mdi-magnify" label="Search"  hide-details>
@@ -658,11 +852,32 @@ export default {
     api: [{api_token:'OiHJO2UfRFlKRNWUJbg5L3hG0CEfQmnkDoW', name:'la fomidable',user_guid: 'TALH5G3I'}],
     DialogOrder:false,
     y: 0,
+
+
+
+
+    itemsPerPageArray: [4, 8, 1000],
+        filter: {},
+        sortDesc: true,
+        page: 1,
+        itemsPerPage: 4,
+        sortBy: '$createdAt',
+        keys: [
+          'phone',
+          'montant',
+          'statut',
+        ],
   }),
 
   computed: {
         formTitle() {
             return this.editedIndex === -1 ? 'Create New Order' : this.readonly?'Order':'Edit Order'
+        },
+        numberOfPages () {
+        return Math.ceil(this.orders.length / this.itemsPerPage)
+        },
+        filteredKeys () {
+            return this.keys.filter(key => key !== 'Name')
         },
     },
 
@@ -675,6 +890,15 @@ export default {
       this.statistics()
   },
   methods: {
+    nextPage () {
+        if (this.page + 1 <= this.numberOfPages) this.page += 1
+      },
+      formerPage () {
+        if (this.page - 1 >= 1) this.page -= 1
+      },
+      updateItemsPerPage (number) {
+        this.itemsPerPage = number
+      },
 
     getDataInitial(){ 
 
