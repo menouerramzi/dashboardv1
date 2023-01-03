abd37c5331aede9375f527dcd8d4155c185b59ef
ziram <menouerramzi0001@gmail.com>
2023-01-03 23:31:58 +0100

    001

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,7 +1,7 @@
 import { account, db, Query } from "../appwrite.js";
 export default function ({ store, redirect }) {
   // Authentication
- // store.commit('auth/SET_UserInfo', localStorage.getItem('cookieFallback'))
+  store.commit('auth/SET_UserInfo', localStorage.getItem('cookieFallback'))
   account.get().then(userData => { 
     store.dispatch('auth/onAuthStateChangedAction', {...userData})
     }).catch(err => {
